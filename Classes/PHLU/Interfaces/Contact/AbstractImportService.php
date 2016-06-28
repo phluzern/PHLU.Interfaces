1eeff792e7b8498c561b6dd718e5fc2f4ab51d3f
Egli Michael <pamegli@n0003574.campus.intern>
2016-06-28 15:21:23 +0200

    initial commit

---
+++ Classes/PHLU/Interfaces/Contact/AbstractImportService.php
@@ -0,0 +1,49 @@
+<?php
+namespace PHLU\Interfaces\Contact;
+
+/*
+ * This file is part of the TYPO3.TYPO3CR package.
+ *
+ * (c) Contributors of the Neos Project - www.neos.io
+ *
+ * This package is Open Source Software. For the full copyright and license
+ * information, please view the LICENSE file which was distributed with this
+ * source code.
+ */
+
+use TYPO3\Flow\Annotations as Flow;
+use PHLU\Neos\Models\Domain\Model\Contact;
+use PHLU\Neos\Models\Service\Contact\ContactService;
+
+/**
+ * A generic Contact Importing Service
+ *
+ * @api
+ * @Flow\Scope("singleton")
+ */
+class AbstractImportService {
+
+
+    /**
+     * @Flow\Inject
+     * @var ContactService
+     */
+    protected $contactService;
+
+
+    /**
+     * Returns a contact model
+     * @param array $data data with associated values
+     * @return Contact
+     * @api
+     */
+    public function createOrUpdateContact($data) {
+
+        return $this->contactService->createOrUpdateContact($this->contactService->getModel($data));
+
+    }
+
+
+
+
+}
